a049b055d84c34fd9e58d6c1dd46a3efae6aaa4f
Sourav2707 <smartsourav33@gmail.com>
2022-07-30 15:17:46 +0530

    UC1_Permutation using recursion

--- DataStructuresAndAlgorithm/Algorithm/Program.cs
+++ DataStructuresAndAlgorithm/Algorithm/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Welcome to Data Structures and Algorithm programs");
+﻿using Algorithm;
+
+Console.WriteLine("Welcome to Data Structures and Algorithm programs");
+String str = "ABC";
+int n = str.Length;
+Algo.permute(str, 0, n - 1);
