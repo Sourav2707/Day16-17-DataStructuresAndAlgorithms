ad6b2327f9d0b8720c7b05a4976dcd1937452c87
Sourav2707 <smartsourav33@gmail.com>
2022-08-07 23:17:38 +0530

    Checking prime number which are anagram/palindrome

--- DataStructuresAndAlgorithm/Algorithm/Algo.cs
+++ DataStructuresAndAlgorithm/Algorithm/Algo.cs
@@ -8,7 +8,7 @@ namespace Algorithm
 {
     public class Algo
     {
-        public static void prime(int num)
+        public static bool prime(int num)
         {
             int Count = 0;                              //counter for number of factors
             for (int i = 2; i < num / 2; i++)
@@ -20,7 +20,43 @@ namespace Algorithm
                 }
             }
             if (Count == 0)
-                Console.WriteLine(num + " ");
+            {
+                return true; //Console.WriteLine(num + " ");
+            }
+            return false;
+        }
+        public static bool palindrome(int num)
+        {
+            int temp = num;
+            int reverse = 0;
+            while (temp != 0)
+            {
+                reverse = reverse * 10 + temp % 10; ;
+                temp /= 10;
+            }
+            if (reverse == num)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static void anagram(String fix, String s)
+        {
+            int l = s.Length;
+            if (l == 1)
+            {
+                Console.WriteLine(fix + s);
+            }
+            else
+            {
+                for (int i = 0; i < l; i++)
+                {
+                    char[] mychar = s.ToCharArray();
+                    char c = mychar[i];
+                    String st = s.Substring(0, i) + s.Substring(i + 1);
+                    anagram(fix + c, st);
+                }
+            }
         }
     }
 }
--- DataStructuresAndAlgorithm/Algorithm/Program.cs
+++ DataStructuresAndAlgorithm/Algorithm/Program.cs
@@ -4,5 +4,8 @@ Console.WriteLine("Hello, to the algorithm and data structure problems");
 Console.WriteLine("Prime numbers between range 0-1000 are -");
 for (int x = 2; x <= 1000; x++)
 {
-    Algo.prime(x);
+    if (Algo.prime(x) == true && Algo.palindrome(x) == true)
+    {
+        Console.WriteLine(x);
+    }
 }
