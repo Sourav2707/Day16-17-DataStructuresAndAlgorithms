e8a802e2f930130b036fa26b60688430af6e59ab
Sourav2707 <smartsourav33@gmail.com>
2022-08-07 22:52:37 +0530

    Print the array in a sorted manner using insertion sort

--- DataStructuresAndAlgorithm/Algorithm/Program.cs
+++ DataStructuresAndAlgorithm/Algorithm/Program.cs
@@ -3,6 +3,4 @@
 Console.WriteLine("Hello, to the algorithm and data structure problems");
 Console.WriteLine("Enter a sentence");
 String s = Console.ReadLine();
-Console.WriteLine("Enter the word you want to search");
-String search = Console.ReadLine();
-Algo.sortAndCall(s, search);
+Algo.insertionSort(s);
